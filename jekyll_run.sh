0d59c1ec747c94e0169de4c4b1544b248ec96abe
EricZhou <1413507308@qq.com>
2018-12-29 18:51:29 +0800

    fix iframe

--- jekyll_run.sh
+++ jekyll_run.sh
@@ -2,6 +2,8 @@
 #--destination _deploy
 
 #scp -Cr _site root@115.28.94.157:/home/tech.mojotv.cn
+rm -rf _config.yml;
+cp _config_code.yml _config.yml;
 
 if [ $1 = 'build' ] ;then
     echo jekyll building;
@@ -13,3 +15,5 @@ if [ $1 = 'serve' ] ;then
     echo jekyll serving;
     bundle exec jekyll serve;
 fi
+rm -rf _config.yml;
+cp _config_tech.yml _config.yml;
