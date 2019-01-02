92b611e26b7ae8108cf015f05859caf59230d995
EricZhou <1413507308@qq.com>
2019-01-02 18:44:50 +0800

    fixed

--- jekyll_run.sh
+++ jekyll_run.sh
@@ -9,6 +9,9 @@ if [ $1 = 'build' ] ;then
     echo jekyll building;
     bundle exec jekyll build;
     #scp -Cr _site root@115.28.94.157:/home/tech.mojotv.cn;
+    #scp -Cr _site root@115.28.94.157:/home/tech.mojotv.cn;
+    curl -H 'Content-Type:text/plain' --data-binary @_site/sitemap.txt "http://data.zz.baidu.com/urls?appid=1573826274415344&token=uQb9Q3G0AFzKOmIM&type=batch";
+    curl -H 'Content-Type:text/plain' --data-binary @_site/sitemap.txt "http://data.zz.baidu.com/urls?appid=1573826274415344&token=uQb9Q3G0AFzKOmIM&type=realtime";
 fi
 
 if [ $1 = 'serve' ] ;then
